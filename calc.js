6a52b01aa11126a0da85fd9ed8e504e41914a79f
Rafael Antunes <rafaelmessias710@gmail.com>
2020-11-06 13:23:53 -0400

    very satisfied

--- calc.js
+++ calc.js
@@ -2,11 +2,12 @@ const calc = {
   form: document.querySelector('[data-calc="calc"]'),
   input: document.querySelector('[data-calc="input"]'),
   nums: document.querySelectorAll('[data-calc="num"]'),
-  operators: document.querySelectorAll('[data-calc="operator"]'),
+  operators: Array.from(document.querySelectorAll('[data-calc="operator"]')),
   result: document.querySelector('[data-calc="result"]'),
   clear: document.querySelector('[data-calc="clear"]'),
   hardClear: document.querySelector('[data-calc="hard-clear"]'),
 };
+let allHistory = [];
 let memory = [];
 
 // colocar o valor do botão no input
@@ -17,23 +18,40 @@ calc.nums.forEach((num) => {
   num.addEventListener("click", putIntoInput);
 });
 
+// Colocar o valor pelo teclado sem estar focado
+const putIntoInputKeyboard = (key) => {
+  calc.input.value += +key;
+};
+
 // limpar imput
 const clearInput = (hard = false) => {
   calc.input.value = "";
   hard && (memory = []);
 };
+// backspace
+const backSpace = () => {
+  const value = Array.from(calc.input.value);
+  value.pop();
+  const newValue = value.join("");
+  calc.input.value = newValue;
+};
+
 calc.clear.addEventListener("click", () => {
+  backSpace();
+});
+calc.clear.addEventListener("dblclick", () => {
   clearInput();
 });
 calc.hardClear.addEventListener("click", () => {
   clearInput(true);
+  calc.input.setAttribute("placeholder", "0");
 });
 
 // selecionar o numero e o tipo de operador e colocar no histórico
-const operation = ({ target }) => {
+const operation = ({ target }, keyType) => {
   memory.push({
     num: +calc.input.value,
-    type: target.value,
+    type: target ? target.value : keyType,
   });
   calc.input.setAttribute("placeholder", +calc.input.value);
   calc.input.value = "";
@@ -64,15 +82,23 @@ const showResult = () => {
       return { num: returned.num * current.num, type: current.type };
   });
   calc.input.value = result.num;
+  allHistory.push(memory);
+  memory = [];
 };
 
 calc.result.addEventListener("click", showResult);
 
 // complementar: executar a funcão operation e show result pelo teclado
+// fase de produção e testes
 window.addEventListener("keyup", ({ key, preventDefault }) => {
-  if (key === "Enter") {
-    showResult();
-  }
+  key === "Enter" && showResult();
+  key === "Backspace" && backSpace();
+  "1, 2, 3, 4, 5, 6, 7, 8, 9".includes(key) && putIntoInputKeyboard(key);
+  const keyType = calc.operators
+    .filter((operator) => operator.innerText === key)
+    .map((item) => item.value);
+  "sum mult divide minus".includes(keyType[0]) && operation({}, keyType[0]);
 });
 
 // complementar: continuar a operação mesmo depois de ter apertado "="
+//  já resolvido
